a07ef8c34ac1ed3cb15494a90c11504b4c807e52
Iulian Margarintescu <iulian@recognos.com>
2014-12-18 11:52:01 +0200

    add descriptions

--- Challenge2/Challenge2/Program.cs
+++ Challenge2/Challenge2/Program.cs
@@ -4,18 +4,31 @@ using System.Threading.Tasks;
 
 namespace Challenge2
 {
+    /// <summary>
+    /// Intro: 
+    /// You need to execute a potentially large number of operations that are IO and CPU intensive (like OCRing a document) and you need to 
+    /// limit the level of concurrency at which the operations are executed. Scheduling to many CPU intensive operations is bad for performance.
+    /// 
+    /// Challenge:
+    /// Modify the code below so that the concurrency level at which RealOperation() is executed never exceeds the value of MaxConcurrency.
+    /// - RealOperation() must be executed TotalNumberOfExecutions times.
+    /// - RealOperation() the max number of concurrent calls to RealOperation() must be MaxConcurrency
+    /// </summary>
     class Program
     {
-        static int counter = 0;
+        const int MaxConcurrency = 5;
+        const int TotalNumberOfExecutions = 20;
+
+        static int currentlyActiveOperations = 0;
+        static int startedOperations = 0;
 
         static void Main(string[] args)
         {
-            const int totalCount = 20;
-            Task[] tasks = new Task[totalCount];
+            Task[] tasks = new Task[TotalNumberOfExecutions];
 
-            for (int i = 0; i < totalCount; i++)
+            for (int i = 0; i < TotalNumberOfExecutions; i++)
             {
-                tasks[i] = Task.Factory.StartNew(() => LongRunningCpuIntensiveOperation()).Unwrap();
+                tasks[i] = Task.Factory.StartNew(() => LongRunningCpuIntensiveOperationAsync()).Unwrap();
             }
 
             Task.WhenAll(tasks).Wait();
@@ -24,12 +37,26 @@ namespace Challenge2
             Console.ReadKey();
         }
 
-        private static async Task LongRunningCpuIntensiveOperation()
+        private static async Task LongRunningCpuIntensiveOperationAsync()
         {
-            var number = Interlocked.Increment(ref counter);
+            await RealOpeartion();
+        }
+
+        // Simulates async work. You can't modify tis method
+        private static async Task RealOpeartion()
+        {
+            Interlocked.Increment(ref currentlyActiveOperations);
+
+            if (currentlyActiveOperations > 5)
+            {
+                throw new InvalidOperationException("Too much concurrency");
+            }
+
+            var number = Interlocked.Increment(ref startedOperations);
             Console.WriteLine("Starting {0}", number);
-            await Task.Delay(1000); // simulate long running
+            await Task.Delay(1000);
             Console.WriteLine("Completed {0}", number);
+            Interlocked.Decrement(ref currentlyActiveOperations);
         }
     }
 }
